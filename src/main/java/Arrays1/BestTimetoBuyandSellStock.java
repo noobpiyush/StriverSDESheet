46fc9619914d815ccd9efde721a31f1292340199
Piyush Waghela <piyush.waghela15514@sakec.ac.in>
2025-03-11 12:45:10 +0530

    done arr part1

---
+++ src/main/java/Arrays1/BestTimetoBuyandSellStock.java
@@ -0,0 +1,34 @@
+package Arrays1;
+
+public class BestTimetoBuyandSellStock {
+
+    public int maxProfitBrute(int[] arr) {
+        int n = arr.length;
+        int maxProfit = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] > arr[i]) {
+                    maxProfit = Math.max(maxProfit, arr[j] - arr[i]);
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+
+
+    public int maxProfit(int arr[]) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        int n = arr.length;
+
+        for (int i = 0;i<n;i++){
+
+            minPrice = Math.min(minPrice,arr[i]);
+            maxProfit = Math.max(maxProfit,arr[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
---
+++ src/main/java/Arrays1/SortColors.java
@@ -0,0 +1,59 @@
+package Arrays1;
+
+public class SortColors {
+
+    public void sortColorsBetter(int[] nums) {
+        int zeroesCnt = 0, onesCnt = 0, twosCnt = 0;
+        int n = nums.length;
+        for(int i = 0;i<n;i++){
+
+            if (nums[i] == 0){
+                zeroesCnt++;
+            } else if (nums[i] == 1) {
+                onesCnt++;
+            } else {
+              twosCnt++;
+            }
+        }
+
+       for (int i = 0;i<zeroesCnt;i++){
+           nums[i] = 0;
+       }
+
+       for (int i = zeroesCnt;i<onesCnt +  zeroesCnt;i++){
+           nums[i] = 1;
+       }
+
+       for (int i = zeroesCnt + onesCnt;i<n;i++){
+           nums[i] = 2;
+       }
+
+
+    }
+    public void sortColorsOptimal(int[] nums) {
+       int n = nums.length;;
+        int low = 0, mid = 0, high = n-1;
+
+        while (mid <= high){
+
+            if (nums[mid] == 0 ){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1){
+                mid++;
+
+            }else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+
+        }
+
+    }
+}
